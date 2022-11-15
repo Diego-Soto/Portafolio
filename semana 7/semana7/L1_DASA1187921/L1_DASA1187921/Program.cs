8499665e90302e0788d35e0bc618726f2d88cccc
Diego-Soto <111150279+Diego-Soto@users.noreply.github.com>
2022-11-14 21:43:42 -0600

    portafolio

---
+++ semana 7/semana7/L1_DASA1187921/L1_DASA1187921/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace L1_DASA1187921
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World ");
+            Console.WriteLine("soy Diego");
+
+            /* la diferencia es que el writeline pone la palabra seleccionada en la siguiente lines*/
+
+            Console.Write("Hello World");
+            Console.Write(" soy Diego");
+            Console.ReadKey();
+
+
+            Console.WriteLine("ingrese su nombre");
+            string nombre = Console.ReadLine();
+
+
+            Console.WriteLine("hola mundo");
+            Console.WriteLine("soy" + nombre);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ semana 7/semana7/T1_DASA1187921/T1_DASA1187921/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace T1_DASA1187921
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi segundo programa");
+            string snombre;
+            string sedad;
+            string scarrera;
+            string scarnet;
+            Console.WriteLine("escriba su nombre:");
+            snombre = Console.ReadLine();
+            Console.WriteLine("escriba su carrera:");
+            scarrera = Console.ReadLine();
+            Console.WriteLine("escriba su edad:");
+            sedad = Console.ReadLine();
+            Console.WriteLine("escriba su carnet:");
+            scarnet = Console.ReadLine();
+
+            Console.WriteLine("nombre:" + snombre);
+            Console.WriteLine("carrera:" + scarrera);
+            Console.WriteLine("edad:" + sedad);
+            Console.WriteLine("carnet:" + scarnet);
+            Console.WriteLine("soy " + snombre+" ,tengo "+ sedad+" años y estudio la carrera de "+ scarrera+" mi numero de carnet es "+scarnet);
+            Console.ReadLine();
+
+        }
+    }
+}
